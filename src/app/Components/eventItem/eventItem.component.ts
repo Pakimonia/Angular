7cc0d7bbd64e02bf104856ab78d1d48ac03b81c1
Annonymous <dev@rivne.itstep.org>
2021-03-29 20:56:45 +0300

    first

---
+++ src/app/Components/eventItem/eventItem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'EventItem',
+  templateUrl: './eventItem.component.html',
+  styleUrls: ['./eventItem.component.css']
+})
+export class EventItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/Components/listEvents/listEvents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ListEvents',
+  templateUrl: './listEvents.component.html',
+  styleUrls: ['./listEvents.component.css']
+})
+export class ListEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
